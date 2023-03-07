3cad8e2fc29322410a273947c72ca6f761011b45
Valerii <tarantino4052@gmail.com>
2023-03-07 17:31:18 +0200

    my version of practice1_video19

---
+++ script.js
@@ -0,0 +1,22 @@
+const numberOfFilms = +prompt('How many films you already watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// const a = prompt("Last film", ''),
+//       b = prompt("Mark? ", ''),
+//       c = prompt("Last film", ''),
+//       d = prompt("Mark? ", '');
+
+// personalMovieDB.movies[a] = b;
+
+for (let i = 0; i < numberOfFilms; i++) {
+    personalMovieDB.movies[prompt("Last film? ", '')] = prompt("Mark? ", '');
+}
+
+console.log(personalMovieDB.movies);
