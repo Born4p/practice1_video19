e1250049ec77d5ba23cedc6b42112ef649a18fa0
Valerii <tarantino4052@gmail.com>
2023-03-09 19:08:23 +0200

    video 21

---
+++ video21.js
@@ -0,0 +1,18 @@
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Great!');
+// } else {
+//     console.log('No!');
+// }
+
+const hamburger = 2;
+const fries = 1;
+const cola = 0;
+
+if (hamburger === 3 && fries === 2 && cola === 1) {
+    console.log('Обираємо цей ресторан');
+} else {
+    console.log('Шукаємо інше');
+}
