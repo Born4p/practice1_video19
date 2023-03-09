f4a4a3a33f6ad66321c47b8d6e0e2160c5acc916
Valerii <tarantino4052@gmail.com>
2023-03-09 22:46:07 +0200

    video 22

---
+++ video22.js
@@ -0,0 +1,29 @@
+// "use strict";
+
+// const num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++;
+// }while (num < 55);
+
+// for (let i = 0; i < 0; i++) {
+//     if (i === 6){
+//        //break;
+//        continue;
+//     }
+    
+    
+//     console.log(i);
+// }
+
+for (let i = 0; i < 10; i++) {
+    if (i%2==0){
+        alert(i);
+    }
+}
