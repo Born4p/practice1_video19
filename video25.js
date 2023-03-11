01a67cc75ec37e5b7a92adeb96a5e33bdbe50694
Valerii <tarantino4052@gmail.com>
2023-03-11 19:25:15 +0200

    video 25 function

---
+++ video25.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function showFirstMessage (text) {
+	console.log(text);
+	
+}
+
+showFirstMessage('Hello world');
+
+// function calc (a, b){
+// 	return (a + b);
+// }
+// console.log(calc(4, 3));
+// console.log(ret());
+// function ret() {
+// 	let num = 50;
+// 	return num;
+// }
+
+let logger = function () {
+	console.log('Hello');
+};
+
+logger();
+
+//const calc = (a, b) => a + b;
